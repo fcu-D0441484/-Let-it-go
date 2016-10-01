90f5213dab45f40f1d852be853412fed50596a0b
子塵 <@192.168.8.105>
2016-10-01 11:41:30 +0800

    作業Let it go

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics=("Let it go!Let it go!Connot hold it back anymore");
+		lyrics=lyrics.replaceAll("it", "\"Her\"");
+		System.out.println(lyrics.replaceAll("Connot", "Can't"));
+	}
+
+}
